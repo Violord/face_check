580aa4535f294a12eaaacccb01d1689e2f2f255c
Violord <hy19930816@163.com>
2019-01-17 15:50:43 +0800

    updated setup

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import find_packages,setup
 setup(
     name = 'face_check',
     version = '0.1',
+    author= 'Violord',
+    author_email= 'hy19930816@163.com',
+    description= 'A small function that compares faces.',
+    license= 'MIT',
+    url= 'https://github.com/Violord/face_check.git',
     packages = find_packages(),
 
     install_requires=['face_recognition>=1.2',
